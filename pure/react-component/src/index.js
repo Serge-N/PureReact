94a155070cded17bb50b45750295b10493374571
Serge-N <nserge05@gmail.com>
2021-08-08 10:58:01 +0200

    React sample
    Basic react sample

--- pure/react-component/src/index.js
+++ pure/react-component/src/index.js
@@ -1,10 +1,15 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
+
+
+
+
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
-    <h1>Main component comming soon</h1>
+  <h1>Main component comming soon</h1>
   </StrictMode>,
   rootElement
 );
