2e9423f99a09a0d1c6f060bc4ddce4717211d0db
Serge Nalishiwa <51508252+Serge-N@users.noreply.github.com>
2021-08-08 10:49:56 +0200

    Test sandbox location test

---
+++ pure/react-component/index.js
@@ -0,0 +1,10 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <h1>Main component comming soon</h1>
+  </StrictMode>,
+  rootElement
+);
