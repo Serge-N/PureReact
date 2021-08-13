04c008ee0fd37800f1edd4d64596e81f6e5db2cc
Serge-N <nserge05@gmail.com>
2021-08-13 22:10:19 +0200

    styling......

--- pure/static-tweet/src/index.js
+++ pure/static-tweet/src/index.js
@@ -8,8 +8,16 @@ function Tweet() {
             <Avatar />
             <div className="content">
                 <Author />
+                <Time />
                 <Message />
+                <div className="buttons">
+                    <ReplyButton />
+                    <RetweetButton />
+                    <LikeButton />
+                    <MoreOptionsButton />
+                </div>
             </div>
+
         </div>
     );
 }
@@ -39,13 +47,13 @@ function Author() {
     );
 }
 
-const Time = () => <span className="time">3h h ago</span>;
+const Time = () => <span className="time">3h ago</span>;
 
 const ReplyButton = () => <i className="fa fa-reply reply-button" />;
 
 const RetweetButton = () => <i className="fa fa-retweet retweet-button" />;
 
-const  LikeButton = () => <i className="fa fa-heart like-button" />;
+const LikeButton = () => <i className="fa fa-heart like-button" />;
 
 const MoreOptionsButton = () => <i className="fa fa-ellipsis-h more-options-button" />;
 
