23b4e3f08988594cb1967891ea32606cff03f24f
Serge-N <nserge05@gmail.com>
2021-09-05 20:11:56 +0200

    file listing with map and key

---
+++ pure/github-file-list/src/index.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import './index.css';
+
+const FileList = ({ files }) => (
+    <table className="file-list">
+        <tbody>
+            {
+                files.map(file => (
+                    <FileListItem key={file.id} file={file} />
+                ))}
+        </tbody>
+    </table>
+);
+
+FileList.propTypes = {
+    files: PropTypes.array.isRequired
+}
+
+
+const FileListItem = ({ file }) => (
+    <tr className="file-list-item" key={file.id}>
+        <td className="file-name">{file.name}</td>
+    </tr>
+);
+
+FileList.propTypes = {
+    file: PropTypes.object.isRequired
+}
+
+const testFiles = [
+    {
+        id: 1,
+        name: 'src',
+        type: 'folder',
+        updated_at: "2021-07-11 21:24:00",
+        latestCommit: {
+            message: 'Initial commit'
+        }
+    },
+    {
+        id: 2,
+        name: 'tests',
+        type: 'folder',
+        updated_at: "2021-07-11 21:24:00",
+        latestCommit: {
+            message: 'Initial commit'
+        }
+    },
+    {
+        id: 3,
+        name: 'README',
+        type: 'file',
+        updated_at: "2021-07-18 21:24:00",
+        latestCommit: {
+            message: 'Added a readme'
+        }
+    },
+];
+
+ReactDOM.render(<FileList files={testFiles} />, document.querySelector('#root'));
