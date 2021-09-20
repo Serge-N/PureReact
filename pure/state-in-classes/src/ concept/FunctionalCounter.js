b3341c5f3860815b732c7ccc59b828d61d4b0e1c
Serge-N <nserge05@gmail.com>
2021-09-20 12:49:52 +0200

    state intro

---
+++ pure/state-in-classes/src/ concept/FunctionalCounter.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function handleAction(event) {
+    console.log("Child did: " + event);
+}
+
+function Parent() {
+    return (
+        <Child onAction={handleAction} />
+    );
+}
+
+function Child({ onAction }) {
+    return (
+        <button onClick={onAction}>
+            Click Me!
+        </button>
+    );
+}
+
+export default Parent;
---
+++ pure/state-in-classes/src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Parent from "./ concept/FunctionalCounter"
+
+ReactDOM.render(<Parent />, document.querySelector('#root'));
