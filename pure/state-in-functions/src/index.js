a1c61f7438a97005c21915dc474a8a0645647096
Serge-N <nserge05@gmail.com>
2021-10-11 17:41:48 +0000

    Hooks introduction.

--- pure/state-in-functions/src/index.js
+++ pure/state-in-functions/src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+
+class OneTimeButton extends React.Component {
+    state = {
+        clicked: false
+    }
+
+    handleClick = () => {
+        this.props.onClick();
+        this.setState({ clicked: true });
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick} disabled={this.state.clicked}>
+                You can only click me once!
+            </button>
+        );
+    }
+}
+
+// button component with hooks
+function OneTimeButtonFunc({ onClick }) {
+    const [clicked, setClicked] = React.useState(false);
+
+    const handleClick = () => {
+        onClick();
+        //OK, no more clicking.
+        setClicked(true);
+    }
+
+    return (
+        <button  onClick={handleClick} disabled={clicked}>
+            You can still click me Once.
+        </button>
+    );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+<span>
+<OneTimeButton onClick={() => alert("hi")} /><br/>
+<OneTimeButtonFunc onClick={() => alert("hi")} />
+</span>
+    , document.querySelector("#root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
