7daa6bb499009fa82d4d07440bfb0641efcce2e2
Serge Nalishiwa <51508252+Serge-N@users.noreply.github.com>
2021-11-11 20:57:20 +0200

    The basic controls.

--- pure/input-controls/README.md
+++ pure/input-controls/README.md
@@ -6,3 +6,6 @@
 
 ## Uncontrolled Inputs
 - Here state is managed by the component itself
+  - Here events can be head so that the value is acquired.
+  - A Ref can also be used.
+  - Refs can only be used with regular elements.
--- pure/input-controls/src/index.js
+++ pure/input-controls/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import ReactDOM from "react-dom";
 
 
@@ -26,17 +26,42 @@ const NoNumbersInput = () => {
         );
     };
     return (
-        <input
+        <input style={{ margin: "10px 1px" }}
             type="text"
             placeholder="No number allowed"
             value={text}
             onChange={handleChange} />);
 }
+
 const Container = () => (
     <div>
         <InputExample />
+        <br />
         <NoNumbersInput />
+        <br />
+        <EasyInput />
+        <br/>
+        <REfInput />
     </div>
 );
+
+const EasyInput = () => (
+    <input type="text" />
+);
+
+const REfInput = () => {
+    const inputRef = useRef();
+
+    const showValue = () => {
+        alert(`Input contains : ${inputRef.current.value}`);
+    };
+
+    return (
+        <div>
+            <input type='text' ref={inputRef} />
+            <button onClick={showValue}>Show Value</button>
+        </div>
+    );
+}
 
 ReactDOM.render(<Container />, document.getElementById("root"));
