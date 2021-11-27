50b170b0a6d3ddb29b3373303c7ad2e9d23b3bdd
Serge Nalishiwa <51508252+Serge-N@users.noreply.github.com>
2021-11-27 17:29:30 +0200

    context api
    all the basics of context api.

--- pure/context-api/src/index.js
+++ pure/context-api/src/index.js
@@ -2,48 +2,65 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const UserContext = React.createContext();
+// using a provider and consumer
+// Context is a way to pass data through the component tree without having to pass props down manually at every level.
+// Context does not have state.
+// There can only be one provider and one consumer per context.
+// Its possible to pass an intial value to the provider.
+// The Consumer component expects a single child.
 
-const UseAvatar = ({ user, size }) => (
-    <img src={user.avatar}
-        alt={user.name}
-        className={`avatar avatar-${size || ""}`} />
+const UserAvatar = ({ size }) => (
+    <UserContext.Consumer>
+        {user => (
+            <img src={user.avatar}
+                alt={user.name}
+                className={`avatar avatar-${size || ""}`} />
+        )}
+    </UserContext.Consumer>
 );
 
-const UserStatus = ({ user }) => (
-    <div className="user-stats">
-        <div>
-            <UseAvatar user={user} size="small" />
-            {user.name}
-        </div>
-        <div className="stats">
-            <div>{UserStatus.followers} Followers</div>
-            <div>Followers {user.following}</div>
-        </div>
-    </div>
+const UserStatus = () => (
+    <UserContext.Consumer>
+        {user => (
+            <div className="user-stats">
+                <div>
+                    <UserAvatar user={user} size="small" />
+                    {user.name}
+                </div>
+                <div className="stats">
+                    <div>{user.followers} Followers</div>
+                    <div>{user.following} Following</div>
+                </div>
+            </div>
+        )}
+    </UserContext.Consumer>
 );
 
 // Accept children and render them
-const Nav = ({children}) => (
+const Nav = () => (
     <div className="nav">
-        {children}
+        <UserAvatar/>
     </div>
 );
 
 const Content = () => (
-    <div className="content">Main Content</div>
-);
-
-
-const Sidebar = ({ children }) => (
-    <div className="sidebar">
-      {children}
+    <div className="content">
+        Main Content here.
     </div>
 );
 
-const Body = ({ sidebar, cfontent }) => (
+
+const Sidebar = () => (
+    <div className="sidebar">
+        <UserStatus />
+    </div>
+);
+
+const Body = () => (
     <div className="body">
-        <Sidebar>{sidebar}</Sidebar>
-        {Content}
+        <Sidebar />
+        <Content />
     </div>
 );
 
@@ -52,18 +69,18 @@ class App extends React.Component {
         user: {
             avatar: "https://avatars.githubusercontent.com/u/51508252?v=4",
             name: "Serge-N",
-            following: "100",
-            followers: "130"
+            following: 100,
+            followers: 130
         }
     };
 
     render() {
         return (
             <div className="app">
-                <Nav>
-                    <UseAvatar user={this.user} size="small"/>
-                </Nav>
-                <Body sidebar={<UserStatus user={this.user}/>}/>
+                <UserContext.Provider value={this.state.user}>
+                    <Nav />
+                    <Body />
+                </UserContext.Provider>
             </div>
         );
     }
